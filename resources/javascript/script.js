53172598e3b97c37a21a490662acfa3007b3dd39
Donnie <108532159+flournoy87@users.noreply.github.com>
2022-08-11 12:35:21 -0400

    add search bar and further adjustments on js for data

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,4 +1,19 @@
+Promise.all([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(id =>
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method:'PATCH',
+        body: JSON.stringify({
+            name: "CS Lewis",
+    }),
+    headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+    },
+})
+        .then((response) => response.json())))
+        .then((json) => console.log(json));
+
+
 function fetchData() {
+    
     fetch('https://jsonplaceholder.typicode.com/posts/').then(response => {
         if (!response.ok) {
             throw Error('ERROR');
@@ -11,7 +26,7 @@ function fetchData() {
             return `
             <div class="post">    
                 <p>Title: ${posts.title}</p>
-                <p>Author: ${posts.userId}</p>
+                <p>Author: ${posts.name}</p>
                 <p>Body: ${posts.body}</p>
             </div>`
         }).join(' ');
@@ -22,6 +37,7 @@ function fetchData() {
         console.log(error);
     })
 }   
-
-
 fetchData();
+
+ 
+    
